afe3467187ab411b05e99b029248b6a385e608db
Puja Rani <pujarani@Pujas-MacBook-Air.local>
2022-09-12 15:32:43 +0530

    query to find publoshed backend courses

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/playground")
+  .connect("mongodb://localhost/mongo-exercises")
   .then(() => console.log("Connected to mongodb"))
   .catch((error) => console.error("Could not connect to mongodb", error));
 
@@ -9,21 +9,12 @@ const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
   tags: [String],
-  date: { type: Date, default: Date.now },
+  date: Date,
   isPublished: Boolean,
+  price: Number,
 });
 
 const Course = mongoose.model("Course", courseSchema);
-async function createCourse() {
-  const course = new Course({
-    name: "Angular course",
-    author: "Mosh",
-    tags: ["angular", "frontend"],
-    isPublished: true,
-  });
-  const result = await course.save();
-  console.log("result", result);
-}
 
 async function getCourses() {
   //eq (equal)
@@ -41,11 +32,10 @@ async function getCourses() {
   const pageNumber = 2;
   const pageSize = 10;
 
-  const courses = await Course.find({ author: "Mosh", isPublished: true })
-    .skip((pageNumber - 1) * pageSize)
-    .limit(pageSize)
+  const courses = await Course.find({ isPublished: true, tags: "backend" })
+
     .sort({ name: 1 })
-    .select({ name: 1, tags: 1 });
+    .select({ name: 1, author: 1 });
   console.log(courses);
 }
 getCourses();
