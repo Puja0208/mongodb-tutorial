7974cbce6cbd023f1482086d1e32e666044b09c0
Puja Rani <pujarani@Pujas-MacBook-Air.local>
2022-09-12 15:19:02 +0530

    feat: Query to find all courses containing word mosh

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ async function getCourses() {
   //and
 
   const courses = await Course.find({ author: /^Mosh/ })
-    .find({ author: /Hamedani$/ })
+    .find({ author: /Hamedani$/i })
+    //contains mosh
+    .find({ author: /.*Mosh.*/i })
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
