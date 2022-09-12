af0c412210a2270fcb67bd4a8c53b0b6720180a1
Puja Rani <pujarani@Pujas-MacBook-Air.local>
2022-09-12 15:10:37 +0530

    feat: QUery to take values from given set of values

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ async function getCourses() {
   //in
   //nin (not in )
 
-  const courses = await Course.find({ price: { $gte: 10, $lte: 20 } })
+  const courses = await Course.find({ price: { $in: [10, 15, 20] } })
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
