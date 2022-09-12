92393d04763041ffb30bb894bd1f5be945becd91
Puja Rani <pujarani@Pujas-MacBook-Air.local>
2022-09-12 15:15:52 +0530

    feat: Using regular expression

--- index.js
+++ index.js
@@ -38,8 +38,7 @@ async function getCourses() {
   //or
   //and
 
-  const courses = await Course.find()
-    .or([{ author: "Mosh" }, { isPublished: false }])
+  const courses = await Course.find({ author: /^Mosh/ })
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
