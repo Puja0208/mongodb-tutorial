48905988714312dabce57ab5502d5f92b6057340
Puja Rani <pujarani@Pujas-MacBook-Air.local>
2022-09-12 15:03:14 +0530

    feat: Querying mongodb database

--- index.js
+++ index.js
@@ -16,12 +16,20 @@ const courseSchema = new mongoose.Schema({
 const Course = mongoose.model("Course", courseSchema);
 async function createCourse() {
   const course = new Course({
-    name: "Node js course",
+    name: "Angular course",
     author: "Mosh",
-    tags: ["node", "backend"],
+    tags: ["angular", "frontend"],
     isPublished: true,
   });
   const result = await course.save();
   console.log("result", result);
 }
-createCourse();
+
+async function getCourses() {
+  const courses = await Course.find({ author: "Mosh", isPublished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  console.log(courses);
+}
+getCourses();
