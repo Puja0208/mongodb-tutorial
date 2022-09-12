bd802e9f5e51dd0c217e604f0f03970484c90ed2
Puja Rani <pujarani@Pujas-MacBook-Air.local>
2022-09-12 15:17:24 +0530

    feat: Query to find courses ending with Hamedani

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ async function getCourses() {
   //and
 
   const courses = await Course.find({ author: /^Mosh/ })
+    .find({ author: /Hamedani$/ })
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
