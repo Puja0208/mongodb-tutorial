2e8e066bce5fcc70a93fc2612adcf2ef84b339e7
Puja Rani <pujarani@Pujas-MacBook-Air.local>
2022-09-12 15:20:46 +0530

    feat: Return count of queried documents

--- index.js
+++ index.js
@@ -38,13 +38,10 @@ async function getCourses() {
   //or
   //and
 
-  const courses = await Course.find({ author: /^Mosh/ })
-    .find({ author: /Hamedani$/i })
-    //contains mosh
-    .find({ author: /.*Mosh.*/i })
+  const courses = await Course.find({ author: "Mosh", isPublished: true })
     .limit(10)
     .sort({ name: 1 })
-    .select({ name: 1, tags: 1 });
+    .count();
   console.log(courses);
 }
 getCourses();
